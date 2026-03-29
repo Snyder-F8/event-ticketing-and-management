0b1343d73df1beddd3acf58e0b7228b5080ad97a
Snyder-F8 <fridahnzomo857@gmail.com>
2026-03-29 13:02:30 +0300

    Updated config python file and app py file

--- server/app.py
+++ server/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 @app.route("/")
 def home():
     return {"message": "Backend is running!"}
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
