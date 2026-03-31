89b8eb9dd1e28babb82162bc3200c550d140a6b1
Snyder-F8 <fridahnzomo857@gmail.com>
2026-03-31 15:25:08 +0300

    Added extensions py file and made changes in app/init file

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,26 +1,25 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from .config import Config
-
-db = SQLAlchemy()
-migrate = Migrate()
+from .extensions import db, migrate
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    # Initialize extensions
     db.init_app(app)
     migrate.init_app(app, db)
 
-    # Import models so Alembic knows about them
+    # Import models so Alembic detects them
     with app.app_context():
-        from .models.user import Role, User
-        from .models.event import Event, TicketType, Image, event_categories
-        from .models.ticket import Ticket, Payment
-        from .models.category import Category
+        from .models import (
+            User, Role,
+            Event, TicketType, Image, event_categories,
+            Ticket, Payment,
+            Category
+        )
 
-    # Basic home route for testing
+    # Basic route
     @app.route("/")
     def home():
         return {"message": "Backend is running successfully!"}
