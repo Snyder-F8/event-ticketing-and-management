445c49e7ba021415db845f063c582b4fe69aafd9
OwinoKevine <owinokevine845@gmail.com>
2026-04-09 15:26:15 +0300

    NodeServer Removed

--- server/app/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from .base import BaseModel
-from .user import Role, User
-from .event import Event, TicketType, Image, event_categories
-from .ticket import Ticket, Payment
-from .category import Category
-
-__all__ = ['BaseModel', 'Role', 'User', 'Event', 'TicketType', 'Image',
-           'Ticket', 'Payment', 'Category', 'event_categories']
