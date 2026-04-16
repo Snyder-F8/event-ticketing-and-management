c53ed66d2dee32a086a9fd79c38e201d900b43b1
Snyder-F8 <fridahnzomo857@gmail.com>
2026-04-16 20:34:09 +0300

    Changed api to API in api.js file

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,9 @@
 // src/services/api.js
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000', // fallback for local dev
-  withCredentials: true,   // keep this if you use cookies/auth
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000",
+  withCredentials: true,
 });
 
 API.interceptors.request.use((req) => {
